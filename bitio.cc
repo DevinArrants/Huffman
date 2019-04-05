4b3c90c6bc092c37bc7bcf68dbe3ee6900900f24
DevinArrants <48366515+DevinArrants@users.noreply.github.com>
2019-04-05 08:44:50 -0700

    Add files via upload

---
+++ bitio.cc
@@ -0,0 +1,42 @@
+//implementation of a class that lets you write or read individual bits from 
+//an actual file. 
+#include "bitio.hh"
+
+//takes one non null pointer to a stream
+//one fo teh pointers passed must be nullptr
+//constructor will save this pointer for subsequent I/O
+BitIO::BitIO(std::ostream* os, std::istream* is){
+        os_ = os;
+        is_ = is;
+        assert( (os || is) && !(os && is));
+        index_ = 0;
+        buffer_ = 0;
+}
+BitIO::~BitIO(){
+}
+//takes a single boolean and outputs bit
+void BitIO::output_bit(bool bit){
+        if(os_){
+            buffer_ <<=  1;
+            buffer_ |= bit;
+            ++index_;
+            if(index_ == 8){
+                    (*os_).put(buffer_);
+                    index_ = 0;
+                    buffer_ = 0;
+            }
+        }
+
+}
+
+//returns true if next bit is of value 1
+//returns false if next bit is of value 0
+bool BitIO::input_bit(){
+    assert(is_); 		//WRONG
+    bool result;
+    if(index_ == 0)      (*is_).get(buffer_);
+    else                index_ += 1;
+    result = buffer_ >> 1 & 1;
+}
+
+
