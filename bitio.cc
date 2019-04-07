81e3a357061550d61e537629ac93e65b0f32991d
BeccaLuff <48332618+BeccaLuff@users.noreply.github.com>
2019-04-07 21:45:00 +0000

    Add files via upload

--- bitio.cc
+++ bitio.cc
@@ -2,15 +2,17 @@
 //an actual file. 
 #include "bitio.hh"
 #include <cassert>
+#include <bitset>
 //takes one non null pointer to a stream
 //one fo teh pointers passed must be nullptr
 //constructor will save this pointer for subsequent I/O
 BitIO::BitIO(std::ostream* os, std::istream* is){
-      auto  os_ = os;
-      auto  is_ = is;
+      os_ = os;
+      is_ = is;
       assert( (os || is) && !(os && is));
-      int index_ = 0;
-      char buffer_ = 0;
+      index_ = 0;
+      if(os_){buffer_ = 0;}
+      else{(*is_).get(buffer_);}
 }
 BitIO::~BitIO(){
     if(os_ && index_!=0){
@@ -20,8 +22,7 @@ BitIO::~BitIO(){
 //takes a single boolean and outputs bit
 void BitIO::output_bit(bool bit){
         if(os_){
-            buffer_ <<=  1;
-            buffer_ |= bit;
+            buffer_ |= (bit<<index_);
             ++index_;
             if(index_ == 8){
                     (*os_).put(buffer_);
@@ -36,9 +37,10 @@ void BitIO::output_bit(bool bit){
 //returns false if next bit is of value 0
 bool BitIO::input_bit(){
     assert(is_);
-    bool result = buffer_ >=128;
-    buffer_<<1;
-    index_++
+    char mask=1;
+    mask=mask<<index_;
+    bool result = (buffer_ & mask)!=0;
+    index_++;
     if(index_ == 8){      
     (*is_).get(buffer_);
     index_=0;
