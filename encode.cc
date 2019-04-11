61e9b0e95f354d199f1d42699325b948f2e069d8
BeccaLuff <48332618+BeccaLuff@users.noreply.github.com>
2019-04-11 00:29:52 +0000

    Add files via upload

---
+++ encode.cc
@@ -0,0 +1,32 @@
+#include "bitio.hh"
+#include "huffman.hh"
+#include <iostream>
+#include <fstream>
+#include <string>
+int main(int argc, char const *argv[])
+{  if(argc<2){ //Make sure filename was provided
+    std::cerr<<"Need input filename to encode\n";
+    return -1;
+  }
+  
+  std::ifstream inp(argv[1]);
+
+  if(!inp.is_open()){ //Make sure file can be opened
+    std::cerr<<"Can't open input file "<<argv[1]<<"\n";
+    return -2; 
+  }
+   std::fstream out; 
+   out.open(std::string(argv[1])+".comp",std::ios::out); 
+   char symbol;
+   Huffman huff;
+   BitIO bitio(&out, nullptr);
+
+  while(inp.get(symbol)){
+    auto bits = huff.encode(symbol);//Encodes symbol
+    for(bool bit : bits){
+      bitio.output_bit(bit); //Sends that code to file.comp
+    }
+  }
+   out.close(); 
+   return 0; 
+} 
